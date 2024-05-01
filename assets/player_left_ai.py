6ad5a4da75252b144abf95fc95f1cabe7f091a29
remboldt <chris.remboldt.cr@gmail.com>
2024-05-01 20:09:31 +0200

    AI performance optimization

--- assets/player_left_ai.py
+++ assets/player_left_ai.py
@@ -1,10 +1,9 @@
-from random import randint
 def move_player_left(player_left_y: int, ball_y: int, points: tuple) -> int:
     # points = (player_left_points, player_right_points)
-    min_speed = 6 + points[1] - points[0]
-    if min_speed >= 8:
-        min_speed = 8
-    speed = randint(min_speed, 9)
+    speed_modifier = 6 + points[1] - points[0]
+    if speed_modifier >= 8:
+        speed_modifier = 8
+    speed = (speed_modifier + 9) / 2
     if player_left_y  < ball_y:
         player_left_y += speed
     elif player_left_y > ball_y:
